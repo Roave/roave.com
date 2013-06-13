138061c4b0749dcff11fe05181eb740ba40bc88e
Aleksey Khudyakov <aleksey@xerkus.pro>
2013-06-13 16:17:54 +1100

    add sxbootstrap

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -44,36 +44,6 @@ return array(
                     ),
                 ),
             ),
-            // The following is a route to simplify getting started creating
-            // new controllers and actions without needing to create a new
-            // module. Simply drop new controllers in, and you can access them
-            // using the path /application/:controller/:action
-            'application' => array(
-                'type'    => 'Literal',
-                'options' => array(
-                    'route'    => '/application',
-                    'defaults' => array(
-                        '__NAMESPACE__' => 'Application\Controller',
-                        'controller'    => 'Index',
-                        'action'        => 'index',
-                    ),
-                ),
-                'may_terminate' => true,
-                'child_routes' => array(
-                    'default' => array(
-                        'type'    => 'Segment',
-                        'options' => array(
-                            'route'    => '/[:controller[/:action]]',
-                            'constraints' => array(
-                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
-                            ),
-                            'defaults' => array(
-                            ),
-                        ),
-                    ),
-                ),
-            ),
         ),
     ),
     'service_manager' => array(
@@ -107,13 +77,29 @@ return array(
         'not_found_template'       => 'error/404',
         'exception_template'       => 'error/index',
         'template_map' => array(
-            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
-            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
-            'error/404'               => __DIR__ . '/../view/error/404.phtml',
-            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+            'layout/layout'            => __DIR__ . '/../view/layout/layout.phtml',
+            'application/index/index'  => __DIR__ . '/../view/application/index/index.phtml',
+            'application/static/about' => __DIR__ . '/../view/application/static/about.phtml',
+            'application/static/team'  => __DIR__ . '/../view/application/static/team.phtml',
+            'error/404'                => __DIR__ . '/../view/error/404.phtml',
+            'error/index'              => __DIR__ . '/../view/error/index.phtml',
         ),
         'template_path_stack' => array(
             __DIR__ . '/../view',
+        ),
+    ),
+    'asset_manager' => array(
+        'resolver_configs' => array(
+            'map' => array(
+                'css/style.css' => __DIR__ . '/../public/css/style.less',
+            ),
+        ),
+        'filters' => array(
+            'css/style.css' => array(
+                array(
+                    'service' => 'SxBootstrap\Service\BootstrapFilter',
+                ),
+            ),
         ),
     ),
 );
