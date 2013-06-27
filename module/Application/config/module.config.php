407d91d36aaa60219a1c65d85db3340368a0a744
Evan Coury <me@evancoury.com>
2013-06-27 16:01:09 -0700

    Fixed link, short array syntax

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -1,98 +1,97 @@
 <?php
 /**
- * Zend Framework (http://framework.zend.com/)
+ * Zend Framework (http://framework.zend.com/]
  *
  * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c] 2005-2013 Zend Technologies USA Inc. (http://www.zend.com]
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
-return array(
-    'router' => array(
-        'routes' => array(
-            'home' => array(
+return [
+    'router' => [
+        'routes' => [
+            'home' => [
                 'type' => 'Literal',
-                'options' => array(
+                'options' => [
                     'route'    => '/',
-                    'defaults' => array(
+                    'defaults' => [
                         'controller' => 'PhlySimplePage\Controller\Page',
                         'template'   => 'application/static/index',
-                    ),
-                ),
-            ),
-            'team' => array(
+                    ],
+                ],
+            ],
+            'team' => [
                 'type' => 'Literal',
-                'options' => array(
+                'options' => [
                     'route'    => '/team',
-                    'defaults' => array(
+                    'defaults' => [
                         'controller' => 'PhlySimplePage\Controller\Page',
                         'template'   => 'application/static/team',
-                    ),
-                ),
-                'may_terminate' => true,
-            ),
-        ),
-    ),
-    'navigation' => array(
-        'default' => array(
-            array(
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'navigation' => [
+        'default' => [
+            [
                 'label' => 'Home',
                 'route' => 'home',
-            ),
-            array(
+            ],
+            [
                 'label' => 'Meet the Team',
                 'route' => 'team',
-            ),
-        ),
-    ),
-    'service_manager' => array(
-         'factories' => array(
+            ],
+        ],
+    ],
+    'service_manager' => [
+         'factories' => [
              'navigation' => 'Zend\Navigation\Service\DefaultNavigationFactory',
-         ),
-        'aliases' => array(
+         ],
+        'aliases' => [
             'translator' => 'MvcTranslator',
-        ),
-    ),
-    'translator' => array(
+        ],
+    ],
+    'translator' => [
         'locale' => 'en_US',
-        'translation_file_patterns' => array(
-            array(
+        'translation_file_patterns' => [
+            [
                 'type'     => 'gettext',
                 'base_dir' => __DIR__ . '/../language',
                 'pattern'  => '%s.mo',
-            ),
-        ),
-    ),
-    'view_manager' => array(
+            ],
+        ],
+    ],
+    'view_manager' => [
         'display_not_found_reason' => true,
         'display_exceptions'       => true,
         'doctype'                  => 'HTML5',
         'not_found_template'       => 'error/404',
         'exception_template'       => 'error/index',
-        'template_map' => array(
+        'template_map' => [
             'layout/layout'            => __DIR__ . '/../view/layout/layout.phtml',
             'application/index/index'  => __DIR__ . '/../view/application/index/index.phtml',
             'application/static/about' => __DIR__ . '/../view/application/static/about.phtml',
             'application/static/team'  => __DIR__ . '/../view/application/static/team.phtml',
             'error/404'                => __DIR__ . '/../view/error/404.phtml',
             'error/index'              => __DIR__ . '/../view/error/index.phtml',
-        ),
-        'template_path_stack' => array(
+        ],
+        'template_path_stack' => [
             __DIR__ . '/../view',
-        ),
-    ),
-    'asset_manager' => array(
-        'resolver_configs' => array(
-            'map' => array(
+        ],
+    ],
+    'asset_manager' => [
+        'resolver_configs' => [
+            'map' => [
                 'css/style.css' => __DIR__ . '/../public/css/style.less',
-            ),
-        ),
-        'filters' => array(
-            'css/style.css' => array(
-                array(
+            ],
+        ],
+        'filters' => [
+            'css/style.css' => [
+                [
                     'service' => 'SxBootstrap\Service\BootstrapFilter',
-                ),
-            ),
-        ),
-    ),
-);
+                ],
+            ],
+        ],
+    ],
+];
