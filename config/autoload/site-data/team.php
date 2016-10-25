10f2956f4f11cb5b3a9e97186e1ed732f485cb22
Evan Coury <me@evancoury.com>
2016-10-25 10:20:43 -0700

    Populated team page

--- config/autoload/site-data/team.php
+++ config/autoload/site-data/team.php
@@ -1,10 +1,118 @@
 <?php
+$team = [];
+
+$team['evan'] = [
+        'name'     => 'Evan Coury (EvanDotPro)',
+        'gravatar' => '3fb997938a45ac4ea89a90164931368d',
+];
+$team['evan']['bio'] = <<<bio
+Evan is a Zend certified engineer for PHP 5.3 and Zend Framework, as well as a regular speaker at developer conferences around the world. He's worked on and led many large-scale web development projects to success. When not in front of a computer, Evan enjoys being outdoors with his two border collies and doing anything related to aviation.
+bio;
+
+$team['marco'] = [
+        'name'     => 'Marco Pivetta (Ocramius)',
+        'gravatar' => '879d7c8ea365a31443e6b890f68a7d7d',
+];
+$team['marco']['bio'] = <<<bio
+Marco is a software developer by day, and a passionate open source software contributor, evangelist and supporter by night. He is part of the Doctrine team and an active Zend Framework 2 contributor. He is constantly looking for new challenges to deal with, especially when it comes to PHP.
+bio;
+
+$team['james'] = [
+        'name'     => 'James Titcumb (asgrim)',
+        'gravatar' => '3a8d5011c49ac9a7702afd3853f5cc30',
+];
+$team['james']['bio'] = <<<bio
+James has been a PHP developer for over 13 years, and has been programming since before that. He's a Zend Certified Engineer, and enjoys working with all things related to PHP. In his spare time he runs the PHP Hampshire user group and the PHP South Coast conference, contributes to various open source projects and occasionally speaks at conferences. He lives in Portsmouth in the UK with his wife and two children.
+bio;
+
+
+$team['ben'] = [
+        'name'     => 'Ben Scholzen (DASPRiD)',
+        'gravatar' => 'c9e618a09b7a872be9376b099b688eda',
+];
+$team['ben']['bio'] = <<<bio
+Ben Scholzen is a Zend Certified Engineer who has been programming since his childhood, and started with PHP in 2000. In 2008 he started contributing to the Zend Framework, developing key components including Zend_Application. He joined the Zend Framework Community Review Team in 2010. For Zend Framework 2 he contributed features such as the new routing system and the internationalization layer.
+bio;
+
+$team['justin'] = [
+        'name'     => 'Justin Martin (FrozenFire)',
+        'gravatar' => 'f9c4bcbe0990d74fbf2043602ec1a60d',
+];
+$team['justin']['bio'] = <<<bio
+<p>Justin Martin is a PHP expert and ZCE with over a decade of highly diverse software development experience. He is recognized in the PHP community for being a <a href="http://www.php.net/manual/en/preface.php">significant contributor</a> to the official PHP manual as well as a founding member of the <a href="http://phpmentoring.org">PHP Mentoring</a> project. Justin is also our resident JavaScript and binary protocol expert.</p>
+<p>Working independently for years, Justin has a wealth of experience in developing custom integrations for the <a href="https://www.mindbodyonline.com/" target="_blank">Mindbody Online</a>&nbsp;studio management system, building large-scale subscription-based video streaming solutions, and custom-flow ecommerce platforms. With Roave, Justin has focused on maintaining the <a href="http://smscloud.com/" target="_blank">SMSCloud</a>&nbsp;platform, as well as providing extensive support for <a href="http://www.callfire.com/" target="_blank">CallFire</a>&nbsp;as their resident PHP expert and custom solutions developer.</p>
+<p>In his spare time, Justin enjoys riding motorcycles, bicycles, <a href="http://rvecafe.com/Assets5/bike01f.jpg">or anything with two wheels</a>.</p>
+bio;
+
+
+$team['rob'] = [
+        'name'     => 'Rob Allen (Akrabat, Nineteen Feet)',
+        'gravatar' => '10e8bec5942c84bf30bcb5af7494f44a',
+];
+$team['rob']['bio'] = <<<bio
+Roave and <a href="http://19ft.com">Nineteen Feet</a> have partnered to form one of the strongest, most qualified PHP and Zend Framework consulting teams available. Rob Allen, of Nineteen Feet Limited, is a software developer and consultant with over 12 years experience in web development using PHP &amp; technologies. He literally wrote the book on Zend Framework. Combining Rob and Evan’s extensive experience and extremely talented teams allows us to ensure we’re always offering the best possible solutions to our clients and take on projects of a much larger scale.
+bio;
+
+$team['nigel'] = [
+        'name'     => 'Nigel Lundsten (nlundsten)',
+        'gravatar' => '816760c449322bb88a02cea027c857ab',
+];
+$team['nigel']['bio'] = <<<bio
+Nigel and Evan have been working together on various projects for well over 10 years. Nigel has extensive experience building complete web applications solving complex problems for a variety of industries.
+bio;
+
+$team['gary'] = [
+        'name'     => 'Gary Hockin (GeeH)',
+        'gravatar' => 'e61ae7dfad370ba47a72f79418160f51',
+];
+$team['gary']['bio'] = <<<bio
+Gary Hockin has been creating code to power web applications for 15 years, the last three of them at an extraordinary level. He is handsome, talented, funny and articulate, and also extremely modest. Gary is a valued contributor to Zend Framework 2 and is a member of the community review team. He's married and has two wonderful children, and when he's not coding, writing about code, speaking about code or reading about code, he can usually be found in the pub playing pool.
+bio;
+
+$team['matthew'] = [
+        'name'     => 'Matthew Setter (settermjd)',
+        'gravatar' => 'bbb7cf7243cb7e468b090f40d59132af',
+];
+$team['matthew']['bio'] = <<<bio
+Matthew is a professional developer with over 10 years experience, and is a Zend Certified Engineer. He’s worked on a wide range of projects in Australia, Germany, and the UK. He’s also a very experienced technical writer, with a wealth of experience writing about a range of topics, including the Zend Framework, JavaScript, Go, Kanban, logging, unit testing, HTML5, Git, mobile development. When he’s not spending time with his wife and little girl, you’ll find him out, exploring somewhere in his adopted homeland of Germany.
+bio;
+
+$team['antoine'] = [
+        'name'     => 'Antoine Hedgecock (mac_nibblet)',
+        'gravatar' => 'cc9476a64cc67c6ca3e5e14ea8061791',
+];
+$team['antoine']['bio'] = <<<bio
+Antoine is a Zend Certified PHP Engineer with extensive expertise and experience with Doctrine, Zend Framework, and REST API design. Antoine is a well-respected member of the Zend Framework community, known for being passionate about application architecture, best practices, and continuous integration.
+bio;
+
+$team['aleksey'] = [
+        'name'     => 'Aleksey Khudyakov (Xerkus)',
+        'gravatar' => '4f7157a6ccf330bc161b126da934cefe',
+];
+$team['aleksey']['bio'] = <<<bio
+<p>Aleksey is a Zend Certified Engineer and Zend Framework contributor, as well as a passionate advocate of clean application development and Domain Driven Design.</p>
+
+<p>As an industry expert with nearly a decade of web development and PHP experience, Aleksey specializes in building complex custom solutions and refactoring enterprise-scale legacy codebases into modern, maintainable applications.</p>
+
+<p>Aside from programming, Aleksey enjoys immersing himself in a good sci-fi novel.</p>
+bio;
+
+$team['priscilla'] = [
+        'name'     => 'Priscilla Hubbard (pdog)',
+        'gravatar' => '8b228094f53356cb826741cc40a01cd6',
+];
+$team['priscilla']['bio'] = <<<bio
+Priscilla keeps everything running smoothly at Roave. She handles everything from client billing to payroll and travel arrangements. In her time off, Priscilla can typically be found spending quality time being tortured by her 2 boys.
+bio;
+
+$team['chloe'] = [
+        'name'     => 'Chloe',
+        'gravatar' => '44e4328250ce6d98d5384c3c1fcd0274',
+];
+$team['chloe']['bio'] = <<<bio
+Chloe has the very, very important role of making sure none of us at Roave work too hard. She has a keen sense of when you're trying very hard to focus on something and will choose that exact moment to drop her ball at your feet.
+bio;
+
 return [
-    'team' => [
-        [
-            'name'     => 'Evan Coury (EvanDotPro)',
-            'gravatar' => '3fb997938a45ac4ea89a90164931368d',
-            'bio'      => "Evan is a Zend certified engineer for PHP 5.3 and Zend Framework, as well as a regular speaker at developer conferences around the world. He's worked on and led many large-scale web development projects to success. When not in front of a computer, Evan enjoys being outdoors with his two border collies and doing anything related to aviation.",
-        ],
-    ],
+    'team' => $team,
 ];
